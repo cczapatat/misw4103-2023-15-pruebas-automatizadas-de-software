125a01c2ad627ba76c33e3744ef5531281a4e15b
Jhon Edinson Muñoz Riaños <je.munozr1@uniandes.edu.co>
2023-11-07 23:55:26 -0500

    Adiciona folder cypress

---
+++ cypress/cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+	projectId:"E2E Ghost tests",
+	screenshotsFolder: 'cypress/screenshots',
+  },
+};
