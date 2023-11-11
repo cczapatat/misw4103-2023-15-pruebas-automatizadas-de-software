07bf6137ca26e72e1a7ece295fd3ea0fd4a4370f
cczapatat <c.zapatat@uniandes.edu.co>
2023-11-11 00:10:31 -0500

    Kraken Scenario 7

--- kraken/features/web/step_definitions/hooks.js
+++ kraken/features/web/step_definitions/hooks.js
@@ -4,7 +4,6 @@ const { WebClient } = require('kraken-node');
 Before(async function() {
   this.deviceClient = new WebClient('chrome', {}, this.userId);
   this.driver = await this.deviceClient.startKrakenForUserId(this.userId);
-  console.log('1111');
 });
 
 After(async function() {
