0c45d0e0926ad178f0bd65f8147d1e9b0aed5dab
cczapatat <c.zapatat@uniandes.edu.co>
2023-11-24 21:25:50 -0500

    Readme Week 7

--- krakenApriori/features/web/step_definitions/step.js
+++ krakenApriori/features/web/step_definitions/step.js
@@ -1,5 +1,4 @@
 const { assert } = require('chai');
-const { faker } = require('@faker-js/faker'); // TODO Eliminar.
 const { Given, When, Then, After, Before } = require('@cucumber/cucumber');
 
 const configProperties = require('../../../kraken.config');
@@ -46,41 +45,11 @@ async function logout(context) {
     await context.driver.url(`${URL_BASE}/signout/`);
 }
 
-async function backToEditorPost(context) {
-    await selectComponent(context, 'button.gh-btn-editor.gh-publish-back-button').click();
-    await wait(1)
-    await context.driver.saveScreenshot(getNamePhoto());
-}
-
-async function addTagToPost(context, tag) {
-    await selectComponent(context, 'button[title="Settings"]').click();
-    await wait(1);
-    await selectComponent(context, '#tag-input').click()
-    await wait(1);
-    await selectComponent(context, '#tag-input').setValue(tag)
-    await wait(1);
-    await selectComponent(context, `li=${tag}`).click();
-    await wait(1);
-    await context.driver.saveScreenshot(getNamePhoto());
-    await wait(1);
-    await selectComponent(context, 'button[title="Settings"]').click();
-    await wait(0.5);
-}
-
 async function createNewTag(context) {
     await selectComponent(context, 'span=New tag').click();
 
     const currentPage = await context.driver.getUrl();
     assert.isTrue(currentPage.includes('tags/new'));
-    await wait(1)
-    await context.driver.saveScreenshot(getNamePhoto());
-}
-
-async function deleteTag(context) {
-    await selectComponent(context, 'span=Delete tag').click();
-    await wait(1)
-    await context.driver.saveScreenshot(getNamePhoto());
-    await selectComponent(context, 'span=Delete').click();
     await wait(1)
     await context.driver.saveScreenshot(getNamePhoto());
 }
@@ -108,19 +77,6 @@ async function deleteAll(context) {
 
 async function goToDashboard(context) {
     await context.driver.url(`${URL_BASE}/dashboard`);
-}
-
-async function searchGlobalAndClick(context, title) {
-    await selectComponent(context, 'button.gh-nav-btn-search').click();
-    await wait(1);
-    await selectComponent(context, 'input[name="selectSearchTerm"].gh-input-with-select-input').setValue(title);
-    await wait(1);
-    await context.driver.saveScreenshot(getNamePhoto());
-    await wait(1);
-    await selectComponent(context, 'li.ember-power-select-option').click();
-    await wait(0.5);
-    await context.driver.saveScreenshot(getNamePhoto());
-    await wait(0.5);
 }
 
 async function newPost(context) {
@@ -164,99 +120,10 @@ async function publishPost(context, total) {
     }
 }
 
-async function schedulePost(context) {
-    await selectComponent(context, 'span=Publish').click();
-    await wait(1);
-    await context.driver.saveScreenshot(getNamePhoto());
-    await wait(1);
-    await selectComponent(context, 'span=Right now').click();
-    await wait(1);
-    await selectComponent(context, 'label=Schedule for later').click();
-    await wait(1);
-    await selectComponent(context, 'button.gh-btn.gh-btn-black.gh-btn-large').click();
-    await wait(1);
-    await selectComponent(context, 'button.gh-btn.gh-btn-large.gh-btn-pulse.ember-view').click();
-    await wait(1);
-    await context.driver.saveScreenshot(getNamePhoto());
-    await wait(1);
-}
-
-async function deletePost(context) {
-    const selectedDelete = version ? 'button.gh-btn.gh-btn-outline.gh-btn-icon.gh-btn-fullwidth' : 'button.gh-btn.gh-btn-hover-red.gh-btn-icon.settings-menu-delete-button';
-    await selectComponent(context, 'button[title="Settings"]').click();
-    await wait(1);
-    await selectComponent(context, selectedDelete).click();
-    await wait(1);
-    await selectComponent(context, 'span=Delete').click();
-}
-
-async function unschedulePost(context) {
-    await selectComponent(context, 'span=Unschedule').click();
-    await wait(1);
-    await context.driver.saveScreenshot(getNamePhoto());
-    await wait(1);
-    await selectComponent(context, 'button.gh-revert-to-draft').click();
-    await wait(1);
-    await context.driver.saveScreenshot(getNamePhoto());
-    await wait(1);
-}
-
 async function setUp(context) {
     await login(context);
     await deleteAll(context);
     await goToDashboard(context);
-}
-
-async function listPublishedPosts(context) {
-    await context.driver.url(`${URL_BASE}/posts?type=published`);
-}
-
-async function listPages(context) {
-    await context.driver.url(`${URL_BASE}/pages`);
-    await wait(1)
-    await context.driver.saveScreenshot(getNamePhoto());
-}
-
-async function listPosts(context) {
-    await context.driver.url(`${URL_BASE}/posts`);
-    await wait(1)
-    await context.driver.saveScreenshot(getNamePhoto());
-}
-
-async function listTags(context) {
-    await context.driver.url(`${URL_BASE}/tags`);
-}
-
-async function listInternalTags(context) {
-    await context.driver.url(`${URL_BASE}/tags?type=internal`);
-    await wait(1);
-    await context.driver.saveScreenshot(getNamePhoto());
-}
-
-async function listPublishedPages(context) {
-    await context.driver.url(`${URL_BASE}/pages?type=published`);
-    await wait(1);
-    await context.driver.saveScreenshot(getNamePhoto());
-}
-
-async function listDraftPages(context) {
-    await context.driver.url(`${URL_BASE}/pages?type=draft`);
-}
-
-async function listScheduledPages(context) {
-    await context.driver.url(`${URL_BASE}/pages?type=scheduled`);
-}
-
-async function listPagesAndCheck(context, page) {
-    const currentPage = await this.driver.getUrl();
-    await context.driver.url(`${URL_BASE}/pages`);
-    assert.isTrue(currentPage.includes('ghost/#/pages'));
-}
-
-async function listDraftPosts(context) {
-    await context.driver.url(`${URL_BASE}/posts?type=draft`);
-    await wait(1);
-    await context.driver.saveScreenshot(getNamePhoto());
 }
 
 async function newPage(context) {
@@ -275,35 +142,6 @@ async function createPage(context, titulo, descripcion) {
     await element.click();
     await wait(1);
     await element.setValue(descripcion);
-    await wait(1);
-    await context.driver.saveScreenshot(getNamePhoto());
-}
-
-async function editPage(context, titulo, descripcion) {
-    await selectComponent(context, 'textarea[placeholder="Page title"]').setValue(titulo)
-    const element = await selectComponent(context, '.koenig-react-editor')
-    await element.click();
-    await wait(1);
-    await element.clearValue();
-    await wait(1);
-    await element.setValue(descripcion);
-
-    await selectComponent(context, 'span=Update').click();
-    await context.driver.url(`${URL_BASE}/pages`);
-}
-
-async function clickFirstPage(context) {
-    let div;
-    let items;
-    if (version) {
-        div = await selectComponent(context, 'div.posts-list.gh-list.feature-memberAttribution');
-        items = await div.$$('div.gh-posts-list-item-group');
-    } else {
-        div = await selectComponent(context, 'ol.gh-list');
-        items = await div.$$('li.gh-posts-list-item');
-    }
-    const pageOne = items[0];
-    await pageOne.click();
     await wait(1);
     await context.driver.saveScreenshot(getNamePhoto());
 }
@@ -328,65 +166,6 @@ async function publishPage(context, total) {
         await wait(1);
         await context.driver.saveScreenshot(getNamePhoto());
     }
-}
-
-async function schedulePage(context) {
-    await selectComponent(context, 'span=Publish').click();
-    await wait(1);
-    await context.driver.saveScreenshot(getNamePhoto());
-    await wait(1);
-    await selectComponent(context, 'span=Right now').click();
-    await wait(1);
-    await selectComponent(context, 'label=Schedule for later').click();
-    await wait(1);
-    await selectComponent(context, 'button.gh-btn.gh-btn-black.gh-btn-large').click();
-    await wait(1);
-    await selectComponent(context, 'button.gh-btn.gh-btn-large.gh-btn-pulse.ember-view').click();
-    await wait(1);
-    await context.driver.saveScreenshot(getNamePhoto());
-    await wait(1);
-}
-
-async function deletePage(context) {
-    const selectedDelete = version ? 'button.gh-btn.gh-btn-outline.gh-btn-icon.gh-btn-fullwidth' : 'button.gh-btn.gh-btn-hover-red.gh-btn-icon.settings-menu-delete-button';
-    await selectComponent(context, 'button[title="Settings"]').click();
-    await wait(1);
-    await context.driver.saveScreenshot(getNamePhoto());
-    await selectComponent(context, selectedDelete).click();
-    await wait(1);
-    await context.driver.saveScreenshot(getNamePhoto());
-    await selectComponent(context, 'span=Delete').click();
-    await wait(1);
-    await context.driver.saveScreenshot(getNamePhoto());
-}
-
-async function unschedulePage(context) {
-    await selectComponent(context, 'span=Unschedule').click();
-    await wait(1);
-    await selectComponent(context, 'button.gh-revert-to-draft').click();
-    await wait(1);
-}
-
-async function addTagToPage(context, tag) {
-    await selectComponent(context, 'button[title="Settings"]').click();
-    await wait(1);
-    await selectComponent(context, '#tag-input').click()
-    await wait(1);
-    await selectComponent(context, '#tag-input').setValue(tag)
-    await wait(1);
-    await selectComponent(context, `li=${tag}`).click();
-    await wait(1);
-    await selectComponent(context, 'button[title="Settings"]').click();
-    await wait(1);
-}
-
-async function publishTag(context) {
-    await selectComponent(context, 'span=Publish').click();
-    await wait(1);
-    await selectComponent(context, 'button.gh-btn.gh-btn-black.gh-btn-large').click();
-    await wait(1);
-    await selectComponent(context, 'button.gh-btn.gh-btn-large.gh-btn-pulse.ember-view').click();
-    await wait(1);
 }
 
 Before(async function () {
@@ -431,12 +210,6 @@ async function createMember(context) {
     await context.driver.saveScreenshot(getNamePhoto());
 }
 
-async function containLastEmail(context) {
-    const component = await selectComponent(context, `.gh-members-list-email`);
-    const text = await component.getText();
-    assert.equal(text, lastCreatedEmail); 
-}
-
 async function deleteMember(context) {
     await selectComponent(context, 'a.ember-view.gh-list-data').click();
     await wait(1);
@@ -472,164 +245,28 @@ When('Admin creates new Post with title {string} and description {string}', asyn
     await createPost(this, title, description);
 });
 
-When('Admin publishes tag', async function () {
-    await publishTag(this);
-});
-
 When('Admin publishes post when total is {int}', async function (total) {
     await publishPost(this, total);
-});
-
-When('Admin schedules post', async function () {
-    await schedulePost(this);
-});
-
-When('Admin clicks {int} post', async function (position) {
-    const fatherItemComponent = version ? 'div.posts-list.gh-list.feature-memberAttribution' : 'ol.posts-list.gh-list';
-    const childItemComponent = version ? 'div.gh-posts-list-item-group' : 'li.gh-list-row.gh-posts-list-item';
-
-    const div = await selectComponent(this, fatherItemComponent);
-    const items = await div.$$(childItemComponent);
-
-    const post = items[position - 1];
-    await post.click();
-    await wait(1);
-    await this.driver.saveScreenshot(getNamePhoto());
-});
-
-When('Admin clicks {int} tag', async function (position) {
-    const ol = await selectComponent(this, 'ol.tags-list.gh-list');
-    const items = await ol.$$('li.gh-tags-list-item');
-    const tag = items[position - 1];
-    await tag.click();
-});
-
-When('Admin clicks to Unschedule', async function () {
-    await unschedulePost(this);
-});
-
-When('Admin clicks Back to editor', async function () {
-    await backToEditorPost(this);
-})
-
-When('Admin list posts', async function () {
-    await listPosts(this);
-});
-
-When('Admin list pages', async function () {
-    await listPages(this);
-});
-
-When('Admin filter published pages', async function () {
-    await listPublishedPages(this);
-});
-
-When('Admin filter draft pages', async function () {
-    await listDraftPages(this);
-});
-
-When('Admin filter scheduled pages', async function () {
-    await listScheduledPages(this);
-});
-
-When('Admin list Pages and check', async function () {
-    await listPagesAndCheck(this);
 });
 
 When('Admin click on New Page', async function () {
     await newPage(this);
 });
 
-When('Admin create a New Page', async function () {
-    const title = faker.person.jobTitle()
-    const description = faker.lorem.paragraphs(2)
-    await createPage(this, title, description);
-});
-
 When('Admin create a New Page with title {string} and description {string}', async function (title, description) {
     await createPage(this, title, description);
-});
-
-When('Admin create a New Page with title {string}', async function (title) {
-    await createPage(this, title, faker.lorem.paragraphs(2));
-});
-
-When('Admin click the first Page', async function () {
-    await clickFirstPage(this);
-});
-
-When('Admin edit an existing Page', async function () {
-    let titulo = faker.person.jobTitle();
-    let descripcion = faker.lorem.paragraphs(2)
-    await editPage(this, titulo, descripcion);
-});
-
-When('Admin add tag {string} to page', async function (tag) {
-    await addTagToPage(this, tag);
-});
-
-When('Admin schedules page', async function () {
-    await schedulePage(this);
-});
-
-When('Admin clicks to Unschedule page', async function () {
-    await unschedulePage(this);
-});
-
-When('Admin filter draft post', async function () {
-    await listDraftPosts(this);
-});
-
-When('Admin filter tags', async function () {
-    await listTags(this);
-});
-
-When('Admin filter internal tags', async function () {
-    await listInternalTags(this);
 });
 
 When('Admin publishes page when total is {int}', async function (total) {
     await publishPage(this, total);
 });
 
-When('Admin clicks to delete page', async function () {
-    await deletePage(this);
-});
-
-When('Admin clicks to delete post', async function () {
-    await deletePost(this);
-});
-
-When('Admin filter published posts', async function () {
-    await listPublishedPosts(this);
-});
-
 When('Admin clicks to new Tag', async function () {
     await createNewTag(this);
 });
 
-When('Admin creates new Tag', async function () {
-    await createTag(this, faker.person.jobTitle(), faker.lorem.paragraph(2));
-});
-
-When('Admin delete a Tag', async function () {
-    await deleteTag(this);
-});
-
 When('Admin creates new Tag with name {string} and description {string}', async function (name, descripcion) {
     await createTag(this, name, descripcion);
-});
-
-When('Admin adds tag {string} to post', async function (tag) {
-    await addTagToPost(this, tag);
-});
-
-When('Admin adds tag {string} to tag', async function (tag) {
-    await addTagToPost(this, tag);
-});
-
-When('Admin searches {string} and click on it', async function (value) {
-    await searchGlobalAndClick(this, value);
 });
 
 Then('Admin visualizes {string} page', async function (pageUrl) {
@@ -667,19 +304,6 @@ Then('Admin sees {int} tags', async function (total) {
     await this.driver.saveScreenshot(getNamePhoto());
 });
 
-Then('Admin sees {int} pages', async function (total) {
-
-    const fatherItemComponent = version ? 'div.posts-list.gh-list.feature-memberAttribution' : 'ol.gh-list';
-    const childItemComponent = version ? 'div.gh-posts-list-item-group' : 'li.gh-list-row.gh-posts-list-item';
-
-    const fatherComponent = await selectComponent(this, fatherItemComponent);
-    const items = await fatherComponent.$$(childItemComponent);
-
-    assert.equal(items.length, total);
-    await wait(1)
-    await this.driver.saveScreenshot(getNamePhoto());
-});
-
 Then('Admin sees {int} pages with title {string}', async function (total, title) {
     const fatherItemComponent = version ? 'div.posts-list.gh-list.feature-memberAttribution' : 'ol.posts-list.gh-list';
     const childItemComponent = version ? 'div.gh-posts-list-item-group' : 'li.gh-list-row.gh-posts-list-item';
@@ -705,11 +329,6 @@ When('Admin clicks on new Member', async function () {
     await createMember(this);
 });
 
-
-Then('Admin sees last created email', async function () {
-    await containLastEmail(this);
-});
-
 Then('Admin sees {int} members', async function (total) {
     const tbody = await selectComponent(this, 'tbody.ember-view', total > 0);
 
@@ -721,7 +340,7 @@ Then('Admin sees {int} members', async function (total) {
         assert.equal(items.length, total);
         await wait(1)
         await this.driver.saveScreenshot(getNamePhoto());
-        
+
         await deleteMember(this)
     } else {
         assert.exists(tbody.error);
